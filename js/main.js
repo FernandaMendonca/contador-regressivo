f5ba34325456557057472d95eb88556ef1c87099
Fernanda Mendonça <fernandasmendonca@outlook.com>
2021-08-08 19:24:34 -0300

    atualizando estilos

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 quandoOBolsonaroSai();
 
 function quandoOBolsonaroSai() {
-    var hoje = new Date().getTime();
-    var diaMaisEsperado = new Date("december 31, 2022 24:00").getTime();
+    let hoje = new Date().getTime();
+    let diaMaisEsperado = new Date("december 31, 2022 24:00").getTime();
+    let segundos = parseInt((diaMaisEsperado - hoje) / 1000);
 
-    var segundos = parseInt((diaMaisEsperado - hoje) / 1000);
-
-    var dias = parseInt(segundos / 86400);
+    let dias = parseInt(segundos / 86400);
     segundos = segundos % 86400;
 
-    var horas = parseInt(segundos / 3600);
+    let horas = parseInt(segundos / 3600);
     segundos = segundos % 3600;
 
-    var minutos = parseInt(segundos / 60);
+    let minutos = parseInt(segundos / 60);
     segundos = parseInt(segundos % 60);
 
-    var tempoRestante = ('Faltam ' + dias + ' dias e ' + horas + ':' + minutos + ':' + segundos);
+    let tempoRestante = `Faltam ${dias} dias e ${horas}: ${minutos}: ${segundos} horas`;
+    
     if (dias >= 0) {
         document.getElementById('contador').innerHTML = tempoRestante;
         setInterval(quandoOBolsonaroSai, 1000);
     } else {
-        document.getElementById('contador').innerHTML = 'Terminou';
+        document.getElementById('contador').innerHTML = 'Fora Bolsonaro!!';
     }
 }
