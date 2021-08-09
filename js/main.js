527f72f804836458c73dec922cff3757ed5dc361
Fernanda Mendonça <fernandasmendonca@outlook.com>
2021-08-08 22:12:38 -0300

    atualizando js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
 quandoOBolsonaroSai();
 
 function quandoOBolsonaroSai() {
-    let hoje = new Date().getTime();
-    let diaMaisEsperado = new Date("december 31, 2022 24:00").getTime();
-    let segundos = parseInt((diaMaisEsperado - hoje) / 1000);
+    const diaMilissegundos = 1000 * 60 * 60 * 24;
+    const hoje = new Date().getTime();
+    const diaMaisEsperado = new Date("12/31/2022").getTime();
 
-    let dias = parseInt(segundos / 86400);
-    segundos = segundos % 86400;
+    const diferencaEntreDatas = diaMaisEsperado - hoje;
 
-    let horas = parseInt(segundos / 3600);
-    segundos = segundos % 3600;
+    const diasRestantes = Math.round(diferencaEntreDatas / diaMilissegundos);
 
-    let minutos = parseInt(segundos / 60);
-    segundos = parseInt(segundos % 60);
+    const tempoRestante = `Faltam ${diasRestantes} dias`;
 
-    let tempoRestante = `Faltam ${dias} dias e ${horas}: ${minutos}: ${segundos} horas`;
-    
-    if (dias >= 0) {
+    if (diasRestantes >= 0) {
         document.getElementById('contador').innerHTML = tempoRestante;
         setInterval(quandoOBolsonaroSai, 1000);
     } else {
